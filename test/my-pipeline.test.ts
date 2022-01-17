5bd25f87af10824af543f2ce8a0c2a012cddf3c2
kdirschl <info@audioshaft.com>
2022-01-17 17:28:22 -0500

    Added environment to test stack

--- test/my-pipeline.test.ts
+++ test/my-pipeline.test.ts
@@ -4,7 +4,12 @@ import * as MyPipeline from '../lib/my-pipeline-stack'
 
 test('Snapshot test', () => {
   const app = new cdk.App()
-  const stack = new MyPipeline.MyPipelineStack(app, 'MyPipelineStack')
+  const stack = new MyPipeline.MyPipelineStack(app, 'MyPipelineStack', {
+    env: {
+      account: '111111111111',
+      region: 'eu-west-1',
+    }
+  })
 
   expect(Template.fromStack(stack)).toMatchSnapshot()
 })
